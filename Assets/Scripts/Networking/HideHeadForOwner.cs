9e3e1756d32f6dca8b6dec0f49a7db6544e9d8bd
ofvarly <omerfaruk_varli21@erdogan.edu.tr>
2025-04-17 22:09:44 +0300

    hide arms for non owners added

--- Assets/Scripts/Networking/HideHeadForOwner.cs
+++ Assets/Scripts/Networking/HideHeadForOwner.cs
@@ -6,6 +6,7 @@ public class HideHeadForOwner : NetworkBehaviour
     [Header("References")]
     [SerializeField] private GameObject headModel; // Oyuncunun kafa modeli
     [SerializeField] private GameObject faceStuff;
+    [SerializeField] private GameObject armModel; // Oyuncunun gövde modeli
 
     public override void OnNetworkSpawn()
     {
@@ -14,12 +15,14 @@ public class HideHeadForOwner : NetworkBehaviour
             // Eğer bu oyuncu kendi karakteriyse, kafa modelini gizle
             headModel.SetActive(false);
             faceStuff.SetActive(false);
+            armModel.SetActive(true); // Kolları gizle
         }
         else
         {
             // Diğer oyuncuların karakterlerinde kafa modeli görünür
             headModel.SetActive(true);
             faceStuff.SetActive(true);
+            armModel.SetActive(false); // Kolları gizle
         }
     }
 }
